1165886ba889d463cea27e99458c5951af3c9d1f
RubenD <rubendariopm14@gmail.com>
2021-09-02 08:36:34 -0400

    updating MyAccount

--- src/components/pages/MyAccount.tsx
+++ src/components/pages/MyAccount.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from "react";
-import s from "./MyInfo.module.scss";
+import s from "./MyAccount.module.scss";
 import { useTheme } from "@common/atoms/Theme";
 import { cnf } from "@common/utils";
 import Input from "@common/atoms/Form/Input";
@@ -13,6 +13,23 @@ import { Link, Redirect, Route, Switch, useRouteMatch } from "react-router-dom";
 import { useState } from "react";
 import { UseLocation, UseRouteMatch } from "@common/atoms/Hooks/routerHooks";
 import { capitalize } from "voca";
+import { useCallback } from "react";
+
+const Payment = () => {
+	return (
+		<Form>
+			<div style={{ display: "inline-flex", flexFlow: "column nowrap", alignItems: "center", maxWidth: 300 }}>
+				<Field name='account_number' label='Account Number' editConfirm className='full-width' />
+				<Field name='routing_number' label='Routing Number' editConfirm className='full-width' />
+				<Field name='payment_method' label='Payment Method' type='select' placeholder='' className='full-width'>
+					<option value='ach'>ACH</option>
+					<option value='check'>Check</option>
+					<option value='wire'>Wire Transfer</option>
+				</Field>
+			</div>
+		</Form>
+	);
+};
 
 export interface MyInfoProps {
 	children?: ReactNode | undefined;
@@ -27,9 +44,69 @@ const MyAccount = ({ className, children, ...props }: MyInfoProps & React.HTMLAt
 	});
 	const { url, path } = useRouteMatch();
 
+	const Section = useCallback(
+		// () =>
+		({ children, header, ...props }) => (
+			<div {...props} className={cnf(s, props.className, "section card")}>
+				{header && <h2 className='margin-top-0'>{header}</h2>}
+				{children}
+			</div>
+		),
+		[]
+	);
+	const [showPayment, setShowPayment] = useState(false);
+
 	return (
-		<div className={className} {...props}>
-			<div style={{ display: "flex" }}>
+		<div className={className}>
+			<Section header='Personal'>
+				<Form state={userData} setState={setUserData}>
+					<div style={{ display: "flex", flexFlow: "column nowrap", alignItems: "center" }} className='padding-2'>
+						<div
+							style={{
+								borderRadius: "50%",
+								width: 80,
+								height: 80,
+								position: "relative",
+								display: "inline-block",
+								overflow: "hidden",
+							}}
+						>
+							<div className={cnf(s, "change-photo")}>Change Photo</div>
+							<Image src={user_icon} alt='Glasses' />
+						</div>
+						{/* <div>{userData.values.name}</div> */}
+						<Field name='name' label='Name' editConfirm />
+
+						<Field name='email' label='Email' editConfirm />
+					</div>
+					{/* <div className='padding-3'>
+						<Field type='file' className='full-width' label='Profile Picture' direction='left' />
+
+						<Button className='border'>Upload</Button>
+					</div> */}
+				</Form>
+			</Section>
+			<Section header='Security'>
+				<Form state={userData} setState={setUserData} initialState={{ values: { password: "123456" } }}>
+					<Field type='password' label='Password' editConfirm disabled name='password' />
+				</Form>
+			</Section>
+			<Section header='Payment'>
+				{showPayment ? (
+					<Payment />
+				) : (
+					<Button
+						className='error-background border'
+						onClick={() => {
+							setShowPayment(true);
+						}}
+					>
+						Show Payment Info
+					</Button>
+				)}
+			</Section>
+
+			{/* <div style={{ display: "flex" }}>
 				<div className='border-right'>
 					<div className='card'>
 						<div style={{ borderRadius: "50%", width: 50, height: 50, display: "inline-block", overflow: "hidden" }}>
@@ -47,7 +124,6 @@ const MyAccount = ({ className, children, ...props }: MyInfoProps & React.HTMLAt
 					</Apply>
 				</div>
 				<div className='text-align-center padding-3' style={{ flexGrow: 1, maxWidth: 400 }}>
-					<UseLocation>{(l) => <h2>{capitalize(l.pathname.split("/").slice(-1)[0])}</h2>}</UseLocation>
 					<Switch>
 						<Route path={`${path}/contact`}>
 							<Form state={userData} setState={setUserData}>
@@ -65,14 +141,14 @@ const MyAccount = ({ className, children, ...props }: MyInfoProps & React.HTMLAt
 						</Route>
 						<Route path={`${path}/settings`}>
 							<Form>
-								{/* <Apply to={Field} depth_max={-1} className="full-width"> */}
+								
 								<div style={{ display: "flex", flexFlow: "column" }}>
 									<Field name='themeForce' type='toggle' label='Force Theme' />
 									<UseForm>
 										{({ state }) => state.values.themeForce && <Field name='theme' type='toggle' label='Theme' />}
 									</UseForm>
 								</div>
-								{/* </Apply> */}
+								
 							</Form>
 						</Route>
 						<Route>
@@ -80,7 +156,7 @@ const MyAccount = ({ className, children, ...props }: MyInfoProps & React.HTMLAt
 						</Route>
 					</Switch>
 				</div>
-			</div>
+			</div> */}
 		</div>
 	);
 };
